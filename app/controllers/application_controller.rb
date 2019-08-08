d744dbac1930a8333e8d1f650c81efb957a5a298
Flavio Bordoni <flavio.bordoni.dev@gmail.com>
2019-08-08 10:29:54 +0000

    Rescue from Pundit::NotAuthorizedError

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   include Pundit
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   def after_sign_in_path_for(resource_or_scope)
     users_path
@@ -14,5 +15,11 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit(:sign_in, keys: [:role])
       devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
       devise_parameter_sanitizer.permit(:account_update, keys: [:role])
+    end
+
+  private
+
+    def user_not_authorized
+      redirect_to request.referrer || root_path, notice: "You are not authorized to perform this action."
     end
 end
