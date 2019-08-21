a272463d8344083c9759edbe1d3fb4f56c6d8731
Flavio Bordoni <flavio.bordoni.dev@gmail.com>
2019-08-21 16:41:30 +0000

    workaround biografy instead of bio

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -90,6 +90,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation, :remember_created_at, :role, :bio, :account_image)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :remember_created_at, :role, :biography, :account_image)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_21_103256) do
+ActiveRecord::Schema.define(version: 2019_08_21_163546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 2019_08_21_103256) do
     t.datetime "updated_at", null: false
     t.integer "role", default: 0
     t.text "bio"
+    t.text "biography"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["role"], name: "index_users_on_role"
