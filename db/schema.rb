b5671807f89ec6c711de1412c1cf38757ee6828a
Flavio Bordoni <flavio.bordoni.dev@gmail.com>
2019-08-24 13:29:41 +0000

    add published to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_23_081343) do
+ActiveRecord::Schema.define(version: 2019_08_24_122631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,8 @@ ActiveRecord::Schema.define(version: 2019_08_23_081343) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.boolean "published", default: false
+    t.datetime "published_at"
     t.index ["content_type"], name: "index_posts_on_content_type"
     t.index ["slug"], name: "index_posts_on_slug", unique: true
     t.index ["user_id"], name: "index_posts_on_user_id"
